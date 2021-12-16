e4ebf4060cf83e0a6eaba9d5d8f75e845ddc1607
Sammuel Reis <sammugr@hotmail.com>
2021-12-15 22:32:34 -0300

    chore: Updates page title using next/head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import styles from "../styles/home.module.scss";
 
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className={styles.title}>
-      <h1>ig.news</h1>
-    </div>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <div className={styles.title}>
+        <h1>ig.news</h1>
+      </div>
+    </>
   );
 }
