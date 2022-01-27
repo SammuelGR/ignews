551dc803ccdfa7a210ca1e72640ca2b6c0bcb32e
Sammuel Reis <sammugr@hotmail.com>
2022-01-26 23:25:25 -0300

    feat: implements github oauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,28 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
-interface SignInButtonProps {
-  isUserLoggedIn?: boolean;
-}
+export function SignInButton() {
+  const { status, data } = useSession();
 
-export function SignInButton({ isUserLoggedIn = true }: SignInButtonProps) {
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return status === "authenticated" ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Sammuel Reis
+      {data.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
