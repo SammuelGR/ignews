e9b6b853cff1a79081553c2730ebf164bbcbea9d
Sammuel Reis <sammugr@hotmail.com>
2022-01-30 00:02:17 -0300

    fix: fixes auth unit test erros
    fixing Header and SignInButton tests

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,6 +2,23 @@ import { render, screen } from "@testing-library/react";
 
 import { Header } from ".";
 
+const fakeSession = {
+  expires: "fake-expires",
+  user: {
+    name: "Sammuel Reis",
+    email: "email@email.com",
+    image: "fake-image",
+  },
+};
+
+jest.mock("next-auth/react", () => {
+  return {
+    useSession() {
+      return fakeSession;
+    },
+  };
+});
+
 describe("Header component", () => {
   it("header renders correctly", () => {
     render(<Header />);
--- src/components/SignInButton/SignInButton.test.tsx
+++ src/components/SignInButton/SignInButton.test.tsx
@@ -1,18 +1,38 @@
 import { render, screen } from "@testing-library/react";
+import { useSession } from "next-auth/react";
+import { mocked } from "ts-jest/utils";
+
 import { SignInButton } from ".";
 
+jest.mock("next-auth/react");
+const mockedUseSession = mocked(useSession);
+
 describe("SignInButton", () => {
+  it("SignInButton renders correctly with unauthenticated user", () => {
+    mockedUseSession.mockReturnValueOnce({
+      data: null,
+      status: "unauthenticated",
+    });
+
+    render(<SignInButton />);
+    expect(screen.getByText("Sign in with Github")).toBeInTheDocument();
+  });
+
   it("SignInButton renders correctly with authenticated user", () => {
+    mockedUseSession.mockReturnValueOnce({
+      data: {
+        expires: "fake-expires",
+        user: {
+          name: "Sammuel Reis",
+          email: "email@email.com",
+          image: "fake-image",
+        },
+      },
+      status: "authenticated",
+    });
+
     render(<SignInButton />);
 
     expect(screen.getByText("Sammuel Reis")).toBeInTheDocument();
   });
-
-  it("SignInButton renders correctly with no authenticated user", () => {
-    render(<SignInButton />);
-
-    expect(screen.getByText("Sign in with Github")).toBeInTheDocument();
-  });
 });
-
-// maldito next auth atualiza quebra teste 😤
